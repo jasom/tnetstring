039ec6eadd48f01588b7524042fba10a1b4bffec
Jason Miller <Aidenn0@geocities.com>
2011-03-21 20:49:01 -0700

    Switch to using string-streams
    It's apparently much faster than using offset-arrays

--- tnetstring.lisp
+++ tnetstring.lisp
@@ -31,57 +31,59 @@
 (defun parse-bin-integer (vector)
   (parse-integer (map 'string #'code-char vector)))
 
-(defun parse-payload (data)
-  (declare (type string data)
-	   (inline slice))
-  (multiple-value-bind (length extra) (find-and-split data #\:)
-    (let* ((length (parse-integer length))
-           (payload (slice extra :end length))
-           (extra (slice extra :start length))
-           (payload-type (aref extra 0))
-           (remain (slice extra :start 1)))
-      (values payload payload-type remain))))
+(defun parse-payload (stream)
+  (declare (type stream stream))
+  (let* 
+      ((length (parse-integer (with-output-to-string (s)
+				(loop for c = (read-char stream)
+				   while (not (eq c #\:))
+				   do (write-char c s)))))
+       (payload (make-string length :initial-element #\Nul))
+       (_ (read-sequence payload stream))
+       (payload-type (read-char stream)))
+    (declare (ignore _))
+    (values payload payload-type)))
 
-(defun parse-tnetstring (data)
+
+(defun parse-tnetstream (stream)
   "Parse a string as netstring"
-  (multiple-value-bind (payload payload-type remain) (parse-payload data)
-    (values (ecase payload-type
-              (#\# (parse-integer payload))
-              (#\" payload)
-              (#\} (parse-dict payload))
-              (#\] (parse-list payload))
-              (#\! (equal payload "true"))
-              (#\~ nil)
-              (#\, payload))
-            remain)))
+  (multiple-value-bind (payload payload-type) (parse-payload stream)
+    (ecase payload-type
+      (#\# (parse-integer payload))
+      (#\" payload)
+      (#\} (parse-dict payload))
+      (#\] (parse-list payload))
+      (#\! (equal payload "true"))
+      (#\~ nil)
+      (#\, payload))))
 
-(defun parse-bytes-as-tnetstring (string)
-  "Parse an array of (unsigned-byte 255) as netstring."
-  (parse-tnetstring (map 'string  #'code-char string)))
+(defun parse-tnetstring (string)
+  (with-input-from-string (s string)
+    (parse-tnetstream s)))
 
 (defun parse-list (data)
   (declare (type string data))
   (if (= (length data) 0)
     nil
-    (loop for (value extra) = (multiple-value-list (parse-tnetstring data))
-          then (multiple-value-list (parse-tnetstring extra))
+    (with-input-from-string (s data)
+      (loop for value = (parse-tnetstream s)
           collect value
-          while (> (length extra) 0))))
+          while (not (null (peek-char nil s nil nil)))))))
 
-(defun parse-pair (data)
-  (multiple-value-bind (key extra) (parse-tnetstring data)
-    (multiple-value-bind (value extra) (parse-tnetstring extra)
-      (values key value extra))))
+(defun parse-pair (stream)
+  (let* ((key (parse-tnetstream stream))
+	 (value (parse-tnetstream stream)))
+      (values key value)))
 
 (defun parse-dict (data)
   (declare (type string data))
   (let ((new-hash (make-hash-table)))
     (if (= (length data) 0)
       (values new-hash data)
-      (loop for (key value extra) = (multiple-value-list (parse-pair data))
-            then (multiple-value-list (parse-pair extra))
+      (with-input-from-string (s data)
+	(loop for (key value) = (multiple-value-list (parse-pair s))
             do (setf (gethash (make-keyword key) new-hash) value)
-            when (<= (length extra) 0) return (values new-hash extra)))))
+            when (null (peek-char nil s nil nil)) return (values new-hash))))))
 
 (defgeneric dump-tnetstring (data &optional stream))
 
@@ -147,14 +149,13 @@
 
 (defun test ()
   (loop for (data expect) in *tests*
-        do (multiple-value-bind (payload remain) (parse-tnetstring data)
-             (assert (= (length remain) 0))
-             (assert (or payload (null expect)))
+     do (let ((payload (parse-tnetstring data)))
+	  (assert (or payload (null expect)))
 
-             (format t "~&~A~&" (equalp payload expect))
-             (format t "EXPECT: ~A GOT: ~A"
-                       (myshow expect)
-                       (myshow payload)))))
+	  (format t "~&~A~&" (equalp payload expect))
+	  (format t "EXPECT: ~A GOT: ~A"
+		  (myshow expect)
+		  (myshow payload)))))
 
 (defun test-dump ()
   (loop for (data expect) in *tests*
